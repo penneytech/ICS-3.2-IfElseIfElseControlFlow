edc43b0b9b2da5560d1ab8c358100528dd8b5f72
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-09 09:22:09 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+You are creating a typing practice app that rates the speed someone can type at. This app will display a message based on the speed. Typically a rate of 90 words per minute is fast, 70 words per minute is average, and 50 and under is slow. 
+
+1) Declare a variable for the typing speed.
+2) Create an if / else if / else control flow statement that satisfies the following requirements:
+    a) Displays one of three messages based on the typing speed.
+    b) The message content should reflect the user achievement based on the speed.
+    c) Set the initial value of the variable you declared to trigger the 'middle' range of achievement. 
+*/
