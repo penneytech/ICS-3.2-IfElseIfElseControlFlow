2c239034ea0034acb8ff66f665d9064d343d543e
penneytech <penneyp@hdsb.ca>
2025-02-06 11:01:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 /*
-You are creating a typing practice app that rates the speed someone can type at. This app will display a message based on the speed. Typically a rate of 90 words per minute is fast, 70 words per minute is average, and 50 and under is slow. 
 
-1) Declare a variable for the typing speed.
-2) Create an if / else if / else control flow statement that satisfies the following requirements:
-    a) Displays one of three messages based on the typing speed.
-    b) The message content should reflect the user achievement based on the speed.
-    c) Set the initial value of the variable you declared to trigger the 'middle' range of achievement. 
+You are creating a typing practice app that rates the speed someone can type at. 
+This app will display a message based on the speed. Here are the speed ranges for the algorithm:
+
+- 50 words per minute or less: "You need to improve your typing speed."
+- 90 words per minute or higher: "You are a speed typing master!"
+- Anything between 50 and 90 words per minute: "You are improving."
+
+1) Declare a variable for the typing speed. Set it's initial value to zero for now.
+
+2) Create an if / else if / else control flow statement that console.logs only one of three above messages based on the typing speed.
+
+3) Change the value of the variable to test the different messages:
+
+- What message is displayed when the speed is 40, 55 and 100? Does the correct message display in these cases?
+- What message is displayed when the speed is 50, 70 and 90? Does the correct message display in these cases?
+
+4) After testing, modify your software to use prompt() to get the typing speed as an input from the user.
+
 */
